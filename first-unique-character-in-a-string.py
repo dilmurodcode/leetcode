cae895edbc45629bd44c07a66d2c2c66e216dafc
Dilmurod <dilmurodcode@gmail.com>
2025-01-30 23:33:48 +0500

    first commit

---
+++ first-unique-character-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        result = {}
+
+        for i in s:
+            if i in result.keys():
+                result[i] = result[i] + 1
+            else:
+                result[i] = 1
+
+        for j in range(len(s)):
+            if result[s[j]] == 1:
+                return j
+
+        return -1
+
+a = Solution(0)
+print(a.firstUniqChar('leetcode'))
